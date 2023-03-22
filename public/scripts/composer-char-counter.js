192b89960306fb48b144d74a6205f271d66d751d
William <Williamtviet@hotmail.com>
2023-03-22 16:15:28 -0700

    added a working counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  console.log("hi");
+  $("textarea").on("input", function() {
+    let remainChars = 140 - $(this).val().length;
+    $(`#counter`).text(remainChars);
+
+    if (remainChars < 0) {
+      $("#counter").addClass("red");
+    }
+
+
+    $("#btn").on('click', function() {
+      console.log(this); //the this word is a ref to button
+    });
+  
+  });
+
+});
--- server/index.js
+++ server/index.js
@@ -32,6 +32,13 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
 
+app.post("/tweets", (req,res) => {
+  console.log("postTest");
+});
+
+
+
+
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
