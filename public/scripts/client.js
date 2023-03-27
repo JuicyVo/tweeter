7d9db057a2bb04b38d3e1caf785a6eed3cd362ae
William <Williamtviet@hotmail.com>
2023-03-27 01:11:20 -0700

    added slidedown error for if a post is too long

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,30 +7,30 @@
 //delete this tweetdata later
 
 $(document).ready(function() {
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 const createTweetElement = function(tweet) {
   let $tweet = $(`
@@ -47,6 +47,7 @@ const createTweetElement = function(tweet) {
       </div>
       <footer class="tweet-footer">
         <div class="datePosted">${new Date(tweet.created_at)}</div>
+        <div class="timeago">${timeago.format(tweet.created_at)}</div>
         <ul class="tweetActions">
           <i class="fa-solid fa-flag"></i>
           <i class="fa-solid fa-retweet"></i>
@@ -71,21 +72,51 @@ const renderTweets = function(tweets) {
   }
 }
 
-// Test / driver code (temporary)
-renderTweets(data);
-
-})
-
-$(document).ready(function() {
-  $("form").submit(function(event){
-    event.preventDefault()
-    let formData =$(this).serialize()
-    console.log (formData)
-    $.ajax({
-      method: "POST",
-      data: formData, 
-      url: "/tweets"
-    
-    })
+const loadTweets=function() {
+  $.ajax({
+    url:'/tweets',
+    method: 'GET',
+    dataType: 'json',
+    success:function(response){
+      renderTweets(response)
+    }
   })
+  console.log ("loadTweets is working")
+}
+loadTweets()
+
+$("form").submit(function(event){
+   event.preventDefault()
+  let formData =$(this).serialize()
+  const errorMessage = document.getElementById("ErrorMessage")
+  if (formData.length >145) {
+    // errorMessage.style.display="flex"
+    $("#ErrorMessage").slideDown()
+    return
+  }
+  console.log (formData)
+  if (formData === "") {
+    alert("Form data is empty");
+    return; //this part seems invalid
+  
+  }
+  $.ajax({
+    method: "POST",
+    data: formData, 
+    url: "/tweets",
+    success:function(response){
+      loadTweets()
+    },
+    error: function(textStatus, errorThrown) {
+      console.log("Error:", textStatus, errorThrown);
+      alert("An error occurred with the form data.");
+    }
+  
+  })
+  errorMessage.style.display="none"
 })
+
+
+})
+
+
